f90410b8bdbd93385fc33069e1c698c049b06b76
mk1971 <wagner.maki@hotmail.com>
2020-06-19 14:07:26 -0300

    Upload And Display Image In ASP.NET Core 3.1

---
+++ MvcCoreUploadAndDisplayImage_Demo/Controllers/HomeController.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MvcCoreUploadAndDisplayImage_Demo.Models;
+using Microsoft.AspNetCore.Hosting;
+using MvcCoreUploadAndDisplayImage_Demo.Data;
+using MvcCoreUploadAndDisplayImage_Demo.ViewModels;
+using Microsoft.EntityFrameworkCore;
+using System.IO;
+
+namespace MvcCoreUploadAndDisplayImage_Demo.Controllers
+{
+    public class HomeController : Controller
+    {
+        /*private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }*/
+
+        private readonly ApplicationDbContext dbContext;
+        private readonly IWebHostEnvironment webHostEnvironment;
+        public HomeController(ApplicationDbContext context, IWebHostEnvironment hostEnvironment)
+        {
+            dbContext = context;
+            webHostEnvironment = hostEnvironment;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var employee = await dbContext.Employees.ToListAsync();
+            return View(employee);
+        }
+
+        public IActionResult New()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> New(EmployeeViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                string uniqueFileName = UploadedFile(model);
+
+                Employee employee = new Employee
+                {
+                    FirstName = model.FirstName,
+                    LastName = model.LastName,
+                    FullName = model.FirstName + " " + model.LastName,
+                    Gender = model.Gender,
+                    Age = model.Age,
+                    Office = model.Office,
+                    Position = model.Position,
+                    Salary = model.Salary,
+                    ProfilePicture = uniqueFileName,
+                };
+                dbContext.Add(employee);
+                await dbContext.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View();
+        }
+
+        private string UploadedFile(EmployeeViewModel model)
+        {
+            string uniqueFileName = null;
+
+            if (model.ProfileImage != null)
+            {
+                string uploadsFolder = Path.Combine(webHostEnvironment.WebRootPath, "images");
+                uniqueFileName = Guid.NewGuid().ToString() + "_" + model.ProfileImage.FileName;
+                string filePath = Path.Combine(uploadsFolder, uniqueFileName);
+                using (var fileStream = new FileStream(filePath, FileMode.Create))
+                {
+                    model.ProfileImage.CopyTo(fileStream);
+                }
+            }
+            return uniqueFileName;
+        }
+    }
+}
