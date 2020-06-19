96f2231dad3944e26fda5a8b420a3bc673c5d129
mk1971 <wagner.maki@hotmail.com>
2020-06-19 09:44:19 -0300

    Validation Message and Validation Summary Tag Helper in ASP.NET MVC Core 3.1

---
+++ ValidationMessageValidationSummaryTagHelper_Demo/Models/Student.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ValidationMessageValidationSummaryTagHelper_Demo.Models
+{
+    public class Student
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Please enter name")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Please choose gender")]
+        public string Gender { get; set; }
+
+        [Required(ErrorMessage = "Please choose date of birth")]
+        [Display(Name = "Date of Birth")]
+        [DataType(DataType.Date)]
+        public DateTime DateofBirth { get; set; }
+
+        [Required(ErrorMessage = "Please enter address")]
+        [StringLength(255)]
+        public string Address { get; set; }
+    }
+}
